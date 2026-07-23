91aedbd437814005224112ec9980e9373c3d02b3
homworkhhh76-rgb <homworkhhh76@gmail.com>
2026-07-23 10:32:56 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,21 +2,12 @@ self.addEventListener('notificationclick', function(event) {
     // إغلاق الإشعار فور الضغط عليه
     event.notification.close();
 
-    // جلب الروابط التي مررناها من ملف HTML
-    const data = event.notification.data;
-    let targetUrl = data.mainUrl; // الرابط الافتراضي عند الضغط على جسم الإشعار
-
-    // التحقق مما إذا كان المستخدم قد ضغط على أحد الأزرار
-    if (event.action === 'btn1' && data.btn1Url) {
-        targetUrl = data.btn1Url;
-    } else if (event.action === 'btn2' && data.btn2Url) {
-        targetUrl = data.btn2Url;
-    }
-
-    // إذا كان هناك رابط فعلي، قم بفتح نافذة المتصفح عليه
-    if (targetUrl) {
+    // جلب الرابط المرفق مع الإشعار وفتحه
+    const urlToOpen = event.notification.data.url;
+    
+    if (urlToOpen) {
         event.waitUntil(
-            clients.openWindow(targetUrl)
+            clients.openWindow(urlToOpen)
         );
     }
 });
